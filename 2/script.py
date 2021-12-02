f5490bc260d2b58a2eb9f9f9e4bc836e97d1c47e
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-02 00:09:06 -0500

    day 2 done

---
+++ 2/script.py
@@ -0,0 +1,38 @@
+file = open("test.txt")
+lines = file.readlines()
+horiz = 0
+dep = 0
+
+for line in lines:
+    splitLine = line.split(' ')
+    instruction = splitLine[0]
+    howMuch = int(splitLine[1])
+    if (instruction == "forward"):
+        horiz += howMuch
+    elif (instruction == "up"):
+        dep -= howMuch
+    else:
+        dep += howMuch
+
+print(horiz*dep)
+
+file = open("test.txt")
+lines = file.readlines()
+
+horiz = 0
+dep = 0
+aim = 0
+
+for line in lines:
+    splitLine = line.split(' ')
+    instruction = splitLine[0]
+    howMuch = int(splitLine[1])
+    if (instruction == "forward"):
+        horiz += howMuch
+        dep += (aim * howMuch)
+    elif (instruction == "up"):
+        aim -= howMuch
+    else:
+        aim += howMuch
+
+print(horiz*dep)
