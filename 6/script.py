2909ce7a4955f177b42081da62dc5bbdf488d3fe
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-06 01:35:41 -0500

    6 done

---
+++ 6/script.py
@@ -0,0 +1,47 @@
+file = open("input.txt")
+
+lines = file.readlines()
+from functools import cache
+
+
+array = []
+for line in lines:
+    parts = line.split(',')
+    for part in parts:
+        array.append(int(part))
+
+#p1 bad
+
+# for i in range(0,256):
+#     print(i)
+#     for v in range(0, len(array)):
+#         if array[v] == 0:
+#             array[v] = 6
+#             array.append(8)
+#         else:
+#             array[v] -= 1    
+
+# print(len(array))
+
+total = 0
+
+@cache
+def recursiveFish(time, days):
+    if days <= 0:
+        return 1
+    elif time == 0:
+        return recursiveFish(6, days-1) + recursiveFish(8, days-1)
+    else:
+        return recursiveFish(time - 1, days-1)
+
+for num in array:
+    total += recursiveFish(num, 80)
+
+print("p1")
+print(total)
+
+for num in array:
+    total += recursiveFish(num, 256)
+
+print("p2")
+print(total)
