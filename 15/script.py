f8b07825de442e685bf0519d4a8e2001411e9d0e
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-15 17:05:44 -0500

    d15 p2 done dijkastra was hard to do

--- 15/script.py
+++ 15/script.py
@@ -1,15 +1,12 @@
-import copy
-import functools
 import heapq
-import sys
 
 file = open("test.txt")
 file = open("input.txt")
 
 lines = file.readlines()
 
-dx = [0,1]
-dy = [1,0]
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
 
 array = []
 results = []
@@ -17,21 +14,39 @@ for line in lines:
     parts = [int(part) for part in line if part != '\n']
     array.append(parts)
 
-@functools.cache
-def recursionIn(thisX, thisY, prevX, prevY, total):
-    if thisX == len(array) - 1 and thisY == len(array[0]) - 1:
-        results.append(total + array[thisX][thisY])
-    else:
-        for x,y in zip(dx, dy):
-            if not (thisX+x == prevX and thisY+y == prevY) and (0 <= thisX+x < len(array) and 0 <= thisY+y < len(array[0])):
-                recursionIn(thisX+x, thisY+y, thisX, thisY, total + array[thisX][thisY])
+def dij(array):
 
-recursionIn(0,0,-1,-1,- array[0][0])
-results.sort(reverse=True)
+    # three arrays
+    results = [[10000] * len(array[0]) for _ in range(len(array))] # for keeping the least number of steps to get to an array slot
+    isVisited = [[False] * len(array[0]) for _ in range(len(array))] # one for keeping track of if we already determined the least number of steps to get to an array slot
+    pq = [] # for keeping track of next min slot to go to
 
-print(results[-1])
+    pq.append([0, (0,0)]) # preprocessing start place so we can kickstart checking the neighbors later
+    results[0][0] = 0 # preprocessing the start, as it doesn't count at all and we don't want anything bad to happen
+
+    heapq.heapify(pq)
+
+    while len(pq) > 0:
+        u = heapq.heappop(pq) # pop out the least first value (the currently lowest num)
+        x = u[1][0]
+        y = u[1][1]
+        if isVisited[x][y]:
+            continue
+        for cx,cy in zip(dx,dy):
+            if 0 <= x+cx < len(array) and 0 <= y+cy < len(array[0]):
+                newDist = results[x][y] + array[x+cx][y+cy] # do 
+                if results[x+cx][y+cy] > newDist:
+                    results[x+cx][y+cy] = newDist
+                    heapq.heappush(pq, [newDist, (x+cx, y+cy)])
+        
+        isVisited[x][y] = True
+
+    return results[-1][-1]
+
+print('part 1:', dij(array))
 
 array = []
+arrayOne = []
 
 for line in lines:
     parts = []
@@ -39,20 +54,13 @@ for line in lines:
         for part in line:
             if part != '\n':
                 parts.append((int(part)+boost-1)%9+1)
-    array.append(parts)
+    arrayOne.append(parts)
 
-arrayOne = copy.deepcopy(array)
-
-for boost in range(1,5):
+for boost in range(0,5):
     for line in arrayOne:
         parts = []
         for part in line:
             parts.append((int(part)+boost-1)%9+1)
         array.append(parts)
 
-results = []    
-
-# recursionIn(0,0,-1,-1,-array[0][0])
-
-# results.sort()
-# print(results[0])
+print('part 2:', dij(array))
