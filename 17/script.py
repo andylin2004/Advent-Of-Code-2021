86a552e234a400693c88c3e9c8bf01c1312033e4
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-17 13:05:39 -0500

    d17 p2 done

--- 17/script.py
+++ 17/script.py
@@ -1,9 +1,7 @@
 file = open("test.txt")
+file = open("input.txt")
 
 lines = file.readline().strip()
-
-xRange = []
-yRange = []
 
 lines = lines.replace('..', '.')
 xStr = lines[lines.index('x=')+2:lines.index(',')]
@@ -16,3 +14,38 @@ xRange[:] = [int(meme) for meme in xRange]
 yRange[:] = [int(meme) for meme in yRange]
 
 print('part 1:', (-1 * yRange[0] -1) * (-1 * yRange[0]) / 2)
+
+pairSets = []
+
+minXWorkingValue = 0
+maxXWorkingValue = xRange[1]
+maxYWorkingValue = (-1 * yRange[0] -1)
+minYWorkingValue = yRange[0]
+
+print(maxYWorkingValue)
+
+while minXWorkingValue * (minXWorkingValue + 1) / 2 < xRange[0]:
+    minXWorkingValue += 1
+
+for initDx in range(minXWorkingValue, maxXWorkingValue+1):
+    for initDy in range(minYWorkingValue, maxYWorkingValue+1):
+        dx = initDx
+        dy = initDy
+        x = 0
+        y = 0
+        while x < xRange[0] or y > yRange[1]:
+            x += dx
+            y += dy
+            dy -= 1
+            dx = max(0, dx-1)
+            if xRange[0] <= x <= xRange[1] and yRange[0] <= y <= yRange[1]:
+                pairSets.append((initDx, initDy))
+                break
+            elif y < yRange[0]:
+                break
+
+total = len(set(pairSets))
+
+print(pairSets)
+
+print(total)
