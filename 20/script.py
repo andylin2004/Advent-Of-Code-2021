0146a92e813e3293ecb60eeb8a4b7d84f00e4c53
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-20 17:45:05 -0500

    d20 more fixing but idk why its broken

--- 20/script.py
+++ 20/script.py
@@ -10,7 +10,6 @@ dy = [-1, 0, 1]
 
 lightIndicator = []
 photo = []
-total = 0
 
 for line in lines:
     if len(lightIndicator) == 0:
@@ -21,7 +20,7 @@ for line in lines:
         photoLine = [x for x in line]
         photo.append(photoLine)
 
-for count in range(2):
+for count in range(50):
     for w in photo:
         w.append('.')
         w.insert(0, '.')
@@ -35,25 +34,19 @@ for count in range(2):
         for v in range(len(photo[i])):
             value = ''
             for cx, cy in itertools.product(dx, dy):
-                if 0 <= i+cx < len(photo) and 0 <= v+cy < len(photo[0]):
-                    if photo[i+cx][v+cy] == '.':
-                        value += '0'
-                    else:
-                        value += '1'
+                if 0 <= i+cx < len(photo) and 0 <= v+cy < len(photo[0]) and photo[i+cx][v+cy] == '#':
+                    value += '1'
                 else:
                     value += '0'
             decimalResult = int(value, 2)
-            print(decimalResult)
             photoNew[i][v] = lightIndicator[decimalResult]
     
-    if count == 1:
-        for line in photoNew:
+    photo = photoNew
+    
+    if count == 1 or count == 49:
+        total = 0
+        for line in photo:
             for x in line:
                 if x == '#':
                     total += 1
-
-    photo = photoNew
-    if count == 1:
-        print(photo)
-
-print(total)
+        print(total)
